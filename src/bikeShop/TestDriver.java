cf8adfe0144e23d69fb537a61fb131af9b3266b1
DillBrowne <browned7@students.rowan.edu>
2015-10-19 18:51:09 -0400

    test

--- src/bikeShop/TestDriver.java
+++ src/bikeShop/TestDriver.java
@@ -6,7 +6,7 @@ public class TestDriver {
 		BikeShopSystem sys = new BikeShopSystem();
 		//BikeShopSystem sys = BikeShopSystem.loadShopState();
 		sys.addItemToInventory(new Accessory(5,10,50,"helmet"));
-		sys.addItemToInventory(new Bike(10,15,500,"bike", "coolBike2000","2000","0922000",Gender.MALE));
+		sys.addItemToInventory(new Bike(10,15,500,"bike", "coolBike","2000","0922000",Gender.MALE));
 		sys.addItemToInventory(new Bike(20,25,1000,"fancy bike", "Awesome Bike 2001","2001","0922000",Gender.FEMALE));
 		sys.newCustomer("Tyler Nolan");
 		sys.saveShopState();
