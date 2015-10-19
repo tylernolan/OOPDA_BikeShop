23226619105d0b760c2e55478e7253e15c7077a5
= <=>
2015-10-19 19:45:27 -0400

    commit to go with the masterGUI

--- src/bikeShop/TestDriver.java
+++ src/bikeShop/TestDriver.java
@@ -9,6 +9,8 @@ public class TestDriver {
 		sys.addItemToInventory(new Bike(10,15,500,"bike", "coolBike2000","2000","0922000",Gender.MALE));
 		sys.addItemToInventory(new Bike(20,25,1000,"fancy bike", "Awesome Bike 2001","2001","0922000",Gender.FEMALE));
 		sys.newCustomer("Tyler Nolan");
+		
+		new MasterGUI(sys);
 		sys.saveShopState();
 		
 		System.out.println(sys.getCustomers().toString());
